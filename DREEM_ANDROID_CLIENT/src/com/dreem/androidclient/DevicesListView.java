44f14f2626280953ff59e6464950c8bd2ec2b612
Yiannis Tofis <yiannistofis@hotmail.com>
2012-01-21 00:14:13 +0200

    Add new views...

---
+++ DREEM_ANDROID_CLIENT/src/com/dreem/androidclient/DevicesListView.java
@@ -0,0 +1,19 @@
+package com.dreem.androidclient;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+public class DevicesListView extends ListActivity {
+    /** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	  super.onCreate(savedInstanceState);
+	  
+	  setListAdapter(new ArrayAdapter<String>(this,
+	          android.R.layout.simple_list_item_1, DEVICES));
+	  getListView().setTextFilterEnabled(true);
+	  }
+	static final String[] DEVICES = new String[] {"WASHING MACHINE", "BOILER", "ETC..."};
+}	
