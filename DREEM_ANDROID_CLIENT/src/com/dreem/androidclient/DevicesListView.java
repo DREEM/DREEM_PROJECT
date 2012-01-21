417365008bb839a1a9b30a7fc611c1a3d37cab03
hasanein <hasanein.ali@gmail.com>
2012-01-21 16:12:14 +0000

    Correcting the activity name in the android manifest file

--- DREEM_ANDROID_CLIENT/src/com/dreem/androidclient/DevicesListView.java
+++ DREEM_ANDROID_CLIENT/src/com/dreem/androidclient/DevicesListView.java
@@ -5,15 +5,18 @@ import android.app.ListActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 
-public class DevicesListView extends ListActivity {
+public class DevicesListView extends ListActivity 
+{
     /** Called when the activity is first created. */
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) 
+	{
 	  super.onCreate(savedInstanceState);
 	  
 	  setListAdapter(new ArrayAdapter<String>(this,
 	          android.R.layout.simple_list_item_1, DEVICES));
 	  getListView().setTextFilterEnabled(true);
-	  }
+	 
+	}
 	static final String[] DEVICES = new String[] {"WASHING MACHINE", "BOILER", "ETC..."};
 }	
